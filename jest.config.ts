a889ebbf524f155531a061762a03e70a8fb8249d
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2025-11-11 13:31:50 -0300

    fix: fazendo downgrade do json-server para versao 0.17 por incompatibilidade/ feat: criando estrutura das pastas de ui

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest'
 
 const config: Config = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
     '^.+\\.[tj]sx?$': [
       'ts-jest',
@@ -11,15 +10,18 @@ const config: Config = {
         tsconfig: {
           module: 'NodeNext',
           moduleResolution: 'NodeNext',
+          isolatedModules: true,
           esModuleInterop: true,
           verbatimModuleSyntax: false,
           jsx: 'react-jsx',
           target: 'ES2022',
           lib: ['ES2022', 'DOM', 'DOM.Iterable'],
+          types: ['jest', 'node', '@testing-library/jest-dom'],
         },
       },
     ],
   },
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
